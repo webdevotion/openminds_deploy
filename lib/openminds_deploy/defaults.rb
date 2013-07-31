52f7192fe681cde93b04a11d1f06173183e993ff
webdevotion <webdevotion>
2013-07-31 10:37:49 +0200

    trying to fix cap 3 compatibility

--- lib/openminds_deploy/defaults.rb
+++ lib/openminds_deploy/defaults.rb
@@ -1,54 +1,55 @@
-configuration = Capistrano.configuration(:must_exist)
+module Capistrano
+  Configuration.instance(true).load do
 
-configuration.load do
-  set :use_sudo, false
-  set :group_writable, false # Shared environment
-  set :keep_releases, 3
-  set :deploy_via, :remote_cache
+    set :use_sudo, false
+    set :group_writable, false # Shared environment
+    set :keep_releases, 3
+    set :deploy_via, :remote_cache
 
-  default_run_options[:pty] = true
+    default_run_options[:pty] = true
 
-  set :deploy_to, "/home/#{user}/apps/#{application}"
+    set :deploy_to, "/home/#{user}/apps/#{application}"
 
-  ssh_options[:forward_agent] = true
+    ssh_options[:forward_agent] = true
 
-  # database.yml: we never keep it in our repository
-  namespace :dbconfig do
-    desc 'Create database.yml in shared/config'
-    task :copy_database_config do
-      run "mkdir -p #{shared_path}/config"
-      put File.read('config/database.yml'), "#{shared_path}/config/database.yml"
-    end
-
-    desc 'Link in the production database.yml'
-    task :link do
-      run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-    end
-  end
-  after 'deploy:finalize_update', 'dbconfig:link'
-  after 'deploy:setup', 'dbconfig:copy_database_config'
-
-  after :deploy, 'deploy:cleanup'
-
-  desc 'Prompts if new migrations are available and runs them if you want to'
-  namespace :deploy do
-    task :needs_migrations, :roles => :db, :only => {:primary => true} do
-      migrations_changed = if previous_release.nil?
-        true # propably first deploy, so no migrations to compare
-      else
-        old_rev = capture("cd #{previous_release} && git log --pretty=format:'%H' -n 1 | cat").strip
-        new_rev = capture("cd #{latest_release} && git log --pretty=format:'%H' -n 1 | cat").strip
-        capture("cd #{latest_release} && git diff #{old_rev} #{new_rev} --name-only | cat").include?('db/migrate')
+    # database.yml: we never keep it in our repository
+    namespace :dbconfig do
+      desc 'Create database.yml in shared/config'
+      task :copy_database_config do
+        run "mkdir -p #{shared_path}/config"
+        put File.read('config/database.yml'), "#{shared_path}/config/database.yml"
       end
-      if migrations_changed && Capistrano::CLI.ui.ask("New migrations pending. Enter 'yes' to run db:migrate") == 'yes'
-        migrate
+
+      desc 'Link in the production database.yml'
+      task :link do
+        run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
       end
     end
-  end
-  after 'deploy:update_code', 'deploy:needs_migrations'
+    after 'deploy:finalize_update', 'dbconfig:link'
+    after 'deploy:setup', 'dbconfig:copy_database_config'
 
-  desc 'Tail application log'
-  task :tail_log, :roles => :app do
-    run "tail -f #{shared_path}/log/#{rails_env}.log"
-  end
+    after :deploy, 'deploy:cleanup'
+
+    desc 'Prompts if new migrations are available and runs them if you want to'
+    namespace :deploy do
+      task :needs_migrations, :roles => :db, :only => {:primary => true} do
+        migrations_changed = if previous_release.nil?
+          true # propably first deploy, so no migrations to compare
+        else
+          old_rev = capture("cd #{previous_release} && git log --pretty=format:'%H' -n 1 | cat").strip
+          new_rev = capture("cd #{latest_release} && git log --pretty=format:'%H' -n 1 | cat").strip
+          capture("cd #{latest_release} && git diff #{old_rev} #{new_rev} --name-only | cat").include?('db/migrate')
+        end
+        if migrations_changed && Capistrano::CLI.ui.ask("New migrations pending. Enter 'yes' to run db:migrate") == 'yes'
+          migrate
+        end
+      end
+    end
+    after 'deploy:update_code', 'deploy:needs_migrations'
+
+    desc 'Tail application log'
+    task :tail_log, :roles => :app do
+      run "tail -f #{shared_path}/log/#{rails_env}.log"
+    end
+  end if Capistrano.const_defined? :Configuration and Capistrano::Configuration.methods.map(&:to_sym).include? :instance
 end
